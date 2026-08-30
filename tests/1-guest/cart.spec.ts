a5593237928d426a546818e4935a84ef585c2c04
JulienBreton <julienbreton@hotmail.fr>
2026-08-30 18:25:58 +0200

    Pour voir le rapport CI avec un test failed

--- tests/1-guest/cart.spec.ts
+++ tests/1-guest/cart.spec.ts
@@ -14,7 +14,7 @@ test.describe('Gestion du panier', () => {
         await productPage.addToCart(2);
 
         await expect(productPage.toastSuccess).toContainText('Produit ajouté au panier.');
-        await expect(productPage.header.cartBadge).toHaveText('2');
+        await expect(productPage.header.cartBadge).toHaveText('3');
     });
 
     test('Vérifier que la quantité maximale autorisée est de 1 pour un produit restreint', async ({ productPage, request }) => {
