338b0c164c3ce81f90ab67b3941faed2b5cf927b
JulienBreton <julienbreton@hotmail.fr>
2026-09-20 16:34:13 +0200

    conf: gére une url pour une ptf de debug

--- config/env.ts
+++ config/env.ts
@@ -1,3 +1,13 @@
-// config/env.ts
-export const API_URL = process.env.API_URL || 'https://api.practicesoftwaretesting.com';
+import dotenv from 'dotenv';
+import path from 'path';
+
+// Charge le .env s'il existe
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
 export const BASE_URL = process.env.BASE_URL || 'https://practicesoftwaretesting.com';
+export const API_URL = process.env.API_URL || 'https://api.practicesoftwaretesting.com';
+
+export const ADMIN_CREDENTIALS = {
+  email: process.env.ADMIN_EMAIL || '',
+  password: process.env.ADMIN_PASSWORD || '',
+};
